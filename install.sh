ff725d08e0520c74b1e693be477b009631a7ce24
kashy01 <87122347+kashy01@users.noreply.github.com>
2021-07-14 18:31:51 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-apt install python3 perl -y ; yum install python3 perl -y ; pip3 install socks pysocks bs4 ; export LANG="en_US.UTF-8"
+apt install python3 python3-pip perl -y ; yum install python3 python3-pip perl -y ; pip3 install socks pysocks bs4 ; export LANG="en_US.UTF-8"
